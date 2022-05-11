49c48d06e4abb9cb6dcbe473bf26e2c09e2b810d
mikhailbildin <38566479+mikhailbildin@users.noreply.github.com>
2022-05-12 00:05:03 +0300

    Delete game directory

--- game/screens.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from pygame import *
-
-class Screen():
-    def __init__(self, size):
-        self.size = size
-        #self.surf = Surface(self.size, SRCALPHA)
-        self.group = sprite.Group()
